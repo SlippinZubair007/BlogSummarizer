66f8fc590d191b5ef00b5ef741726f6e3f9e0d0b
Muhammad Zubair Khan <detroitzubairkhan123@gmail.com>
2025-07-08 23:53:13 +0500

    feat
    Basic skeleton code setup -  Day 1

---
+++ blogsumm/src/lib/utils.ts
@@ -0,0 +1,11 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+// Zcvl2pyvPX5vOUoX
+
+// mongodb+srv://detroitzubairkhan123:Zcvl2pyvPX5vOUoX@cluster0.dhce7st.mongodb.net/blog_db?retryWrites=true&w=majority&appName=Cluster0
